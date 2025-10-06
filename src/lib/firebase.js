05c3f8f6f257a01b7a8c8754185d03681142f35b
Ashwani Baghel <ashwanibaghel826@gmail.com>
2025-10-06 16:51:44 +0000

    lwkin yarrrr firebase studio se to jab website ko hum live karte hai to

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,10 +7,10 @@ import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "firebase-studio-1758486484312",
+  "projectId": "studio-6552995429-8bffe",
   "appId": "1:901130035292:web:6986494d18fbe805c5c699",
   "apiKey": "AIzaSyBDf2QxMRQgS3KGI6iRMZFBg5iFfZ4uK3g",
-  "authDomain": "firebase-studio-1758486484312.firebaseapp.com",
+  "authDomain": "studio-6552995429-8bffe.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "901130035292"
 };
