85c5a8bee50f49a76439eb2ebc31e684c3a91703
Ashwani Baghel <ashwanibaghel826@gmail.com>
2025-10-07 16:30:50 +0000

    Subject: Redesign the entire application with a new, "Appetizing" Color

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-transparent text-white hover:bg-white hover:text-black",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
