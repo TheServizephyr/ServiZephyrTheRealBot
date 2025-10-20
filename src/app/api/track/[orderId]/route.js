1456307bfe99d0b2b46c94f15a561ef98be3f090
Ashwani Baghel <ashwanibaghel826@gmail.com>
2025-10-20 17:47:01 +0000

    23:05:59.464 Running build in Portland, USA (West) – pdx1 23:05:59.465 B

--- src/app/api/track/[orderId]/route.js
+++
@@ -1,3 +0,0 @@
-// This file is no longer needed as the tracking page now fetches data directly from Firestore using client-side hooks.
-// Keeping it might cause confusion. It's better to rely on the real-time capabilities of Firestore on the client.
-// We can delete this file.
