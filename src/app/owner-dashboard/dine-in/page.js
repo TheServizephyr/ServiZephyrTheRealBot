3eb19abdd9c627e539a3808cd0fd93ac1daa1f3c
UTKARSH PATEL <utkarshp2003@gmail.com>
2025-10-22 15:07:03 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: AlertTr

--- src/app/owner-dashboard/dine-in/page.js
+++ src/app/owner-dashboard/dine-in/page.js
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { RefreshCw, Printer, CheckCircle, IndianRupee, Users, Clock, ShoppingBag, Bell, MoreVertical, Trash2, QrCode, Download, Save, Wind, Edit, Table as TableIcon, History, Search, PlusCircle, Salad, Fork, Spoon, Droplet } from 'lucide-react';
+import { RefreshCw, Printer, CheckCircle, IndianRupee, Users, Clock, ShoppingBag, Bell, MoreVertical, Trash2, QrCode, Download, Save, Wind, Edit, Table as TableIcon, History, Search, PlusCircle, Salad, Fork, Spoon, Droplet, AlertTriangle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { auth } from '@/lib/firebase';
@@ -1074,5 +1074,3 @@ function DineInPage() {
 }
 
 export default DineInPage;
-
-    
