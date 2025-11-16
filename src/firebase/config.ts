f2e8632cd731d322add77f505123253585bec61d
Ashwani Baghel <ashwanibaghel826@gmail.com>
2025-11-16 08:37:44 +0000

    bhai ab kyun firebase ki permission  ka error hai bhai jab humne isko ga

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   "appId": "1:901130035292:web:6986494d18fbe805c5c699",
   "apiKey": "AIzaSyBDf2QxMRQgS3KGI6iRMZFBg5iFfZ4uK3g",
   "authDomain": "studio-6552995429-8bffe.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "901130035292"
+  "measurementId": "G-402W82K4Y9",
+  "messagingSenderId": "901130035292",
+  "storageBucket": "studio-6552995429-8bffe.appspot.com"
 };
-
